370a1395c123153830fd62d56079853c6e3fb07a
Woosung Lim <wml5207@psu.edu>
2020-09-19 12:46:45 +0000

    //

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Author: Woosung Lim wml5207@psu.edu
 
 def digit_sum(n):
-   return 0 if n == 0 else int(n%10) + digit_sum(int(n/10)) 
+   return 0 if n == 0 else int(n%10) + digit_sum(int(n//10)) 
 
 if __name__ == "__main__":
   num=int(input("Enter an int: "))
