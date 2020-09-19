eebbe1e1b16d43008efc2ba9f74403a72b7b9575
Woosung Lim <wml5207@psu.edu>
2020-09-19 12:32:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Woosung Lim wml5207@psu.edu
+
+def digit_sum(n):
+   return 0 if n == 0 else int(n%10) + digit_sum(int(n/10)) 
+
+if __name__ == "__main__":
+  num=int(input("Enter an int: "))
+  print(f"sum of digits of {num} is {digit_sum(num)}.")
